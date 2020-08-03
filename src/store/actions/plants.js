df3ccfd5cbc326db64fa09115a8615e67bb88a80
Byanca De la fuente <byancadlf@gmail.com>
2020-08-03 11:10:25 -0500

    added action creators

--- src/store/actions/plants.js
+++ src/store/actions/plants.js
@@ -4,4 +4,22 @@ import {
   GET_PLANTS_FAILURE
 } from "../constants";
 
-export const getPlants = () => {};
+export const getPlants = () => {
+  return {
+    type: GET_PLANTS
+  };
+};
+
+export const getPlantsSuccess = plant => {
+  return {
+    type: GET_PLANTS_SUCCESS,
+    payload: plant
+  };
+};
+
+export const getPlantsFailure = e => {
+  return {
+    type: GET_PLANTS_FAILURE,
+    payload: e
+  };
+};
